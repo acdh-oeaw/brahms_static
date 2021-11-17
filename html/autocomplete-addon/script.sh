5dbebbce2df1316e6c505c4c60e2c04996471f69
Daniel_Stoxreiter <daniel.stoxreiter@oeaw.ac.at>
2021-11-17 11:33:37 +0100

    autocomplete-addon added

---
+++ html/autocomplete-addon/script.sh
@@ -0,0 +1,35 @@
+# bin/bash
+
+echo "delete .git"
+rm -rf .git
+
+echo "reading config.txt"
+if [ -f ./config.txt ]
+    then
+        array=()
+        while IFS= read -r line; do
+            array+=($line)
+        done < config.txt
+    else
+        echo "config.txt not found!"
+fi
+
+projectdir=${array[1]}
+inputdir=${array[2]}/${array[3]}
+outputdir="autocomplete-addon"
+filename="${array[0]}.txt"
+
+cd ..
+if [ -f $outputdir/$filename ]
+    then
+        echo "$outputdir/$filename already exists; delete"
+        rm -f "$outputdir/$filename"
+fi
+
+echo "create file: $filename"
+touch "$outputdir/$filename"
+echo "open inputdir: $inputdir"
+for file in $inputdir/*.json; do
+    echo $file | sed -e "s_${projectdir}/__g" >> $outputdir/$filename
+done
+echo "$filename in $outputdir created"
