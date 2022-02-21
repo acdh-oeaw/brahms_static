c1ef7613c0a92957354b89a61a02ace988758f44
linxOD <linxOD@users.noreply.github.com>
2022-02-21 12:23:30 +0000

    deploy: 3c2b311d43ed12c78664ee4bb8f24f95412f0443

---
+++ js/dt-regular.js
@@ -0,0 +1,32 @@
+function createDataTable( tableID, pageLength, start ) {
+    var table = $(  '#' + tableID ).DataTable(
+        {
+            dom: "'<'row controlwrapper'<'col-sm-4'f><'col-sm-4'i><'col-sm-4 exportbuttons'Br>>'" +
+                "'<'row'<'col-sm-12't>>'" +
+                "'<'row'<'col-sm-6 offset-sm-6'p>>'",
+            responsive: true,
+            "pageLength": pageLength,
+            buttons: [{
+                extend: 'copyHtml5',
+                text: '<i class="far fa-copy"/>',
+                titleAttr: 'Copy',
+                className: 'btn-link',
+            },
+            {
+                extend: 'excelHtml5',
+                text: '<i class="far fa-file-excel"/>',
+                titleAttr: 'Excel',
+                className: 'btn-link',
+            },
+            {
+                extend: 'pdfHtml5',
+                text: '<i class="far fa-file-pdf"/>',
+                titleAttr: 'PDF',
+                className: 'btn-link',
+            }],
+            // order: order,
+            orderCellsTop: true,
+            fixedHeader: true,
+        }
+    );
+}    
