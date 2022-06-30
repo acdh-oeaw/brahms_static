f1853e0a5b7f442be9665aa113115fb27983d58a
BrahmsOnline <BrahmsOnline@users.noreply.github.com>
2022-06-30 15:48:19 +0000

    deploy: 88b3ae0e18032d851398d75471ff9ab037f55a81

--- js/dt-opus-filter.js
+++ js/dt-opus-filter.js
@@ -64,6 +64,10 @@ function bin ( data1, data2 ) {
 function createDataTable( tableID ) {
     var table = $(  '#' + tableID ).DataTable(
         {
+            language: {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.19/i18n/German.json"
+        },
+            
             dom: "'<'row controlwrapper'<'col-sm-4'f><'col-sm-4'i><'col-sm-4 exportbuttons'Br>>'" +
                 "'<'row'<'col-sm-12't>>'" +
                 "'<'row'<'col-sm-6 offset-sm-6'p>>'",
